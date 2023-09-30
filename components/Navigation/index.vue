2616acbaf2073ceb89a5253047cf87b5a45c9ef0
alimpp <hmdmpp@gmail.com>
2023-09-30 10:37:55 -0700

    Add new chnages in style

--- components/Navigation/index.vue
+++ components/Navigation/index.vue
@@ -1,28 +1,54 @@
 <template>
     <div 
-    class="navigation"
+     class="navigation"
     >
         <div 
-        class="d-flex w-50 px-4 py-1"
+          class="logo-container px-4 py-1"
         >
           <AppLogo />
           <div 
-          class="d-flex justify-center align-center px-2"
+           class="d-flex justify-center align-center px-2"
           >
             <v-icon class="app_pointer"
             >mdi-cart-outline</v-icon>
           </div>
           <div 
-          class="d-flex justify-center align-center"
+            class="d-flex justify-center align-center"
           >
             <ThemeSwichter />
           </div>
         </div>
+        <div class="items-container">
+            <span
+              :class="{'app_active_route': Route.path === '/home'}" 
+              class="app_pointer app-font-size-14 app-font-weight-600 px-2 py-2"
+            >
+              Home
+            </span>
+            <span
+              :class="{'app_active_route': Route.path === '/about'}" 
+              class="app_pointer app-font-size-14 app-font-weight-600 px-2 py-2"
+            >
+              About Us
+            </span>
+           <span
+            :class="{'app_active_route': Route.path === '/contact'}"
+            class="app_pointer app-font-size-14 app-font-weight-600 px-2 py-2"
+          >
+              Contact Us
+            </span>
+            <span
+              :class="{'app_active_route': Route.path === '/products'}" 
+              class="app_pointer app-font-size-14 app-font-weight-600 px-2 py-2"
+            >
+              Products
+            </span>
+        </div>
         <div 
-        class="d-flex justify-end align-center w-50 px-4"
+          class="auth-container px-4"
         >
          <div 
-         class="d-flex align-center"
+          class="d-flex align-center"
          >
           <CoreBtn 
             class="mx-2"
@@ -44,10 +70,43 @@
     </div>
 </template>
 
+<script setup>
+import { useRoute } from 'vue-router'
+
+const Route = useRoute()
+</script>
+
 <style lang="scss">
 .navigation{
     width: 100%;
     display: flex;
     box-shadow: 0 0 2px 0 #fd7403a2;
+    .logo-container{
+      width: 20%;
+      display: flex;
+    }
+    .items-container{
+      width: 50%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    .auth-container{
+      width: 30%;
+      display: flex;
+      justify-content: end;
+      align-items: center;
+    }
+    @media (max-width: 800px) {
+      .logo-container{
+        width: 50%;
+      }
+      .items-container{
+        display: none;
+      }
+      .auth-container{
+        width: 50%;
+      }
+    }
 }
 </style>
