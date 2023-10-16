a51080150ef90f8f8d6e0a6e094f72439e394c8c
alimpp <hmdmpp@gmail.com>
2023-10-16 09:45:44 -0700

    Add core divider

---
+++ components/CoreDivider/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="d-flex flex-column">
+       <span class="app-font-size-16 app-font-weight-600">
+          {{ name }}
+       </span>
+       <div 
+        class="app-bg-primary line"  
+        :style="{'width': `${width}`}"
+       >
+       </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    name : {
+        type : String,
+        default : "",
+        required : false
+    },
+    width : {
+        type : String,
+        default : "",
+        required : false
+    },
+})
+</script>
+
+<style lang="scss">
+.line{
+    height: 2px;
+    margin-top: 5px;
+}
+</style>
