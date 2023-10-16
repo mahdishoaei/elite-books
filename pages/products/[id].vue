b2491e6d77f1acc45533cf7861a43b9ed30a0378
mahdi shoaei <mahdishoaei98@gmail.com>
2023-10-17 00:15:59 +0330

    make product detail ui

---
+++ pages/products/[id].vue
@@ -0,0 +1,18 @@
+<template>
+<div class="app-container ">
+
+    <div class="mt-10">
+        <div class="d-flex justify-center">
+          <CoreDivider name="Book Details" width="92px" />
+        </div >
+        <ProductDetail class="mt-10"/>
+    </div>
+    <div class="mt-10">
+        <div class="d-flex justify-center">
+          <CoreDivider name="Similar Books" width="101px" />
+        </div>
+        <SuggestSlider />
+    </div>
+
+</div>
+</template>
